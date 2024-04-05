f4332b1c9ac5a5c0dfe2fbeca55c12e774e83e41
PastryPolisher <6962702+PastryPolisher@users.noreply.github.com>
2024-04-05 16:12:19 -0400

    Set up base app

---
+++ src/components/ProductBody.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="productBody">
+        product body here
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+.productBody {
+    background-color: var(--ft-ptw-light);
+    position: relative;
+	display: flex;
+	justify-content: space-around;
+	height: calc(100vh - var(--header-height));
+	height: calc(100dvh - var(--header-height));
+	width: 100%
+}
+</style>
